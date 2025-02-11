9f1466d188169ec65dd02a1f43cc9f320c339cad
costantine-tarimo <112546624+costantine-tarimo@users.noreply.github.com>
2025-02-11 17:56:18 +0300

    Refactor constants for consistency and reuse.
    Renamed a constant for better readability and updated repeated string values to reference existing constants. This improves code maintainability and reduces duplication, ensuring more consistent responses.

--- app/Helper/Constants.php
+++ app/Helper/Constants.php
@@ -19,7 +19,7 @@ class Constants
     public const SAVE_REPRINTING_DETAILS = '0032';
     public const CARDLESS_WITHDRAW = '1012';
     public const AGENT_STATEMENT = '1025';
-    public const CustomerQueryControlNumberDetails = '1026';
+    public const CUSTOMER_QUERY_CTL_DETAILS = '1026';
     public const DATA_MAP = '1027';
     public const NIDA = '1028';
     public const ACCOUNT_OPENING = '1029';
@@ -30,15 +30,15 @@ class Constants
      * RESPONSE MESSAGES
     */
     public const INVALID_CREDENTIALS = 'Invalid Credentials';
-    public const FAILED_VALIDATING_IMEI = 'Invalid Credentials';
+    public const FAILED_VALIDATING_IMEI = self::INVALID_CREDENTIALS;
     public const OPERATOR_INACTIVE_SUSPENDED = 'Account Locked';// OPERATOR/AGENT IS INACTIVE/SUSPENDED
-    public const OPERATOR_NOT_EXIST = 'Invalid Credentials';
-    public const AGENT_NOT_EXIST = 'Invalid Credentials';
+    public const OPERATOR_NOT_EXIST = self::INVALID_CREDENTIALS;
+    public const AGENT_NOT_EXIST = self::INVALID_CREDENTIALS;
     public const AGENT_USERNAME_REQUIRED = 'Agent Username Required';
     public const FAILED_TO_GET_KEY = 'Bad Request';
     public const SUCCESS_OPERATOR_PIN_CHANGE = 'PIN Changed Successfully';
-    public const NO_MATCHING_RECORDS = 'Invalid Credentials';
-    public const DECRYPTION_FAILED = 'Bad Request';
+    public const NO_MATCHING_RECORDS = self::INVALID_CREDENTIALS;
+    public const DECRYPTION_FAILED = self::FAILED_TO_GET_KEY;
     public const DEVICE_INACTIVE_SUSPENDED = 'Device Locked';
 
 }
