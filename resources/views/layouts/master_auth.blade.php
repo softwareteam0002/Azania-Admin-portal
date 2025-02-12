c9f28e1c1dc6ca461a6c53292c25a09d1fec4479
costantine-tarimo <112546624+costantine-tarimo@users.noreply.github.com>
2025-02-12 18:16:32 +0300

    Update master_auth layout for streamlined structure
    Revised the title to "Azania: Admin Portal" for clarity and updated the `auth-body` structure by removing redundant HTML. Simplified the layout to focus on yielding dynamic content and eliminated unnecessary error notification logic.

--- resources/views/layouts/master_auth.blade.php
+++ resources/views/layouts/master_auth.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Welcome')</title>
+    <title>@yield('title', 'Azania: Admin Portal')</title>
     <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
     <script src="{{ asset('public/js/jquery.min.js') }}"></script>
     <script src="{{ asset('public/js/ajax-jquery.min.js') }}"></script>
@@ -17,45 +17,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet"/>
 </head>
 
-<body class="login-bg">
-<div>
-    <div class="login-cont">
-        <div class="col-md-8">
-            <div class="card-group">
-                <div class="card p-4 border-0">
-                    <div class="card-body">
-                        <div class="head-logo">
-                            <img src="{{ asset('public/images/azania-logo.png') }}" class="client-logo" alt="MHB Logo">
-                        </div>
-                        <div class="pt-2 ">
-                            @if ($errors->any())
-                                @foreach ($errors->all() as $error)
-                                    <div class="alert alert-danger text-center">
-                                        {{ $error }}
-                                    </div>
-                                @endforeach
-                            @else
-                                @if(Session::has('color'))
-                                    <div
-                                        class="alert alert-{{ Session::get('color') }} alert-dismissible fade show text-center"
-                                        role="alert">
-                                        {{ Session::get('notification') }}
-                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                @endif
-                            @endif
-                        </div>
-
-                        @yield('content')
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<body class="auth-body">
+@yield('content')
 
 <script src="{{ asset('public/js/custom.js') }}"></script>
 </body>
